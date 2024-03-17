af8abc0155061b283565af043318c786bf4aecb0
sh66n <shaanedroos85@gmail.com>
2024-03-17 17:30:09 +0530

    Played around with shadcn a bit

--- client/src/routes/FilesRoute.jsx
+++ client/src/routes/FilesRoute.jsx
@@ -1,20 +1,78 @@
 import React from "react";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Calendar } from "@/components/ui/calendar";
+import { Button } from "@/components/ui/button";
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function FilesRoute() {
+    // const [date, setDate] = (React.useState < Date) | (undefined > new Date());
+
     return (
         <div>
-            <h1>pdf</h1>
-            <iframe
-                src="https://res.cloudinary.com/ddmjczaqq/image/upload/v1710658831/Axis/pjbv8iddqsl4rxziwf1y.pdf"
-                width="800"
-                height="500"
-            ></iframe>
-            <object
-                data="https://res.cloudinary.com/ddmjczaqq/image/upload/v1710658831/Axis/pjbv8iddqsl4rxziwf1y.pdf"
-                type="application/pdf"
-                height="500"
-                width="500"
-            ></object>
+            <Accordion type="single" collapsible>
+                <AccordionItem value="item-1">
+                    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                    <AccordionContent>
+                        Yes. It adheres to the WAI-ARIA design pattern.
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
+            <Calendar
+                mode="single"
+                // selected={date}
+                // onSelect={setDate}
+                className="rounded-md border"
+            />
+            <Button>Click me</Button>
+            <HoverCard>
+                <HoverCardTrigger>Hover</HoverCardTrigger>
+                <HoverCardContent>
+                    The React Framework – created and maintained by @vercel.
+                </HoverCardContent>
+            </HoverCard>
+            <Sheet>
+                <SheetTrigger>Open</SheetTrigger>
+                <SheetContent>
+                    <SheetHeader>
+                        <SheetTitle>Are you absolutely sure?</SheetTitle>
+                        <SheetDescription>
+                            This action cannot be undone. This will permanently
+                            delete your account and remove your data from our
+                            servers.
+                        </SheetDescription>
+                    </SheetHeader>
+                </SheetContent>
+            </Sheet>
+            <Tabs defaultValue="account" className="w-[400px]">
+                <TabsList>
+                    <TabsTrigger value="account">Account</TabsTrigger>
+                    <TabsTrigger value="password">Password</TabsTrigger>
+                </TabsList>
+                <TabsContent value="account">
+                    Make changes to your account here.
+                </TabsContent>
+                <TabsContent value="password">
+                    Change your password here.
+                </TabsContent>
+            </Tabs>
         </div>
     );
 }
